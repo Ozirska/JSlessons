576e011b10e87f44fd6ca5ed92543db711428656
Ozirska <m.ozirska2000@gmail.com>
2022-06-02 09:51:32 +0200

    chenged typy of function

--- lesson21/index.js
+++ lesson21/index.js
@@ -1,12 +1,14 @@
 
-       export function getTitleElement(){
+       export const getTitleElement = () => {
        const titleElem = document.querySelector(.title)
        console.dir(titleElem)
        }
 
-      export function getInputElement () {
+      export const getInputElement = () => {
       const inputElement = document.querySelector('input')
      
       console.dir(inputElement)
     }
+    getTitleElement()
+    getInputElement()
     
