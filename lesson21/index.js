073b63be70f399205c7a04a3e182896c2a100d14
Ozirska <m.ozirska2000@gmail.com>
2022-06-02 22:18:53 +0200

    add return

--- lesson21/index.js
+++ lesson21/index.js
@@ -1,12 +1,14 @@
 export const getTitleElement = () => {
   const titleElem = document.querySelector(".title");
   console.dir(titleElem);
+  return titleElem;
 };
 
 export const getInputElement = () => {
   const inputElement = document.querySelector("input", ".text");
 
   console.dir(inputElement);
+  return inputElement;
 };
 getTitleElement();
 getInputElement();
