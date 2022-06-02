a36df1bbd5adcf85f32aced419bea883c2ac3d87
Ozirska <m.ozirska2000@gmail.com>
2022-06-02 12:29:35 +0200

    fixed syntax errors

--- lesson21/index.js
+++ lesson21/index.js
@@ -1,14 +1,12 @@
+export const getTitleElement = () => {
+  const titleElem = document.querySelector(".title");
+  console.dir(titleElem);
+};
 
-       export const getTitleElement = () => {
-       const titleElem = document.querySelector(.title)
-       console.dir(titleElem)
-       }
+export const getInputElement = () => {
+  const inputElement = document.querySelector("input", ".text");
 
-      export const getInputElement = () => {
-      const inputElement = document.querySelector('input')
-     
-      console.dir(inputElement)
-    }
-    getTitleElement()
-    getInputElement()
-    
+  console.dir(inputElement);
+};
+getTitleElement();
+getInputElement();
