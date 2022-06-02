ff4d53c4ffb7c79984056082066bf7defc65ffcb
Ozirska <m.ozirska2000@gmail.com>
2022-06-02 22:40:05 +0200

    ussed querySelectorAll

---
+++ lesson21/task2/index.js
@@ -0,0 +1,12 @@
+export const getItemsList = () => {
+  const elementsList = document.querySelectorAll(".technology");
+  console.dir(elementsList);
+  return elementsList;
+};
+
+export const getItemsArray = () => {
+  const elementsArray = document.querySelectorAll(".tool");
+
+  console.dir(Array.from("elementsArray"));
+  return Array.from("elementsArray");
+};
