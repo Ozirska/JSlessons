be1cac1d8f6a5f70da0c0231fe2efe508b41a44f
Ozirska <m.ozirska2000@gmail.com>
2022-06-14 22:52:17 +0200

    event target

--- lesson22/task2/index.js
+++ lesson22/task2/index.js
@@ -2,4 +2,4 @@ const checkbox = document.querySelectorAll(".task-status");
 const check = (event) => {
   console.log(event.target.checked);
 };
-[...checkbox].map((el) => el.addEventListener("click", check));
+[...checkbox].map((el) => el.addEventListener("change", check));
