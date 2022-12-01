d45dcbcc423fbfe054e1ab3efa7a50646c20d198
Ozirska <m.ozirska2000@gmail.com>
2022-12-01 20:32:34 +0100

    c

--- lesson28/task69_2/index.js
+++ lesson28/task69_2/index.js
@@ -2,11 +2,15 @@ import { addImage } from "./addImage.js";
 
 export const addImageV2 = (url) => {
   // put your code here
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    resolve(addImage(url, reject));
+  });
 };
 
 // examples
 
-addImageV2("https://server.com/image.png")
+addImageV2(
+  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
+)
   .then((data) => console.log(data)) // ==> { width: 200, height: 100 }
   .catch((error) => console.log(error)); // ==> 'Image load failed'
