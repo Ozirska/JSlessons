b14be6522e8692bc3a6353dca64cfb78b415613e
Ozirska <m.ozirska2000@gmail.com>
2022-12-21 10:46:14 +0100

    get data from Github

---
+++ lesson33/index.js
@@ -0,0 +1,27 @@
+const userAvatarElem = document.querySelector(".user__avatar");
+const userNameElem = document.querySelector(".user__name");
+const userLocationElem = document.querySelector(".user__location");
+
+const fetchUserData = (userName) => {
+  return fetch(`https://api.github.com/users/${userName}`).then((response) =>
+    response.json()
+  );
+};
+
+renderUserDada = (userData) => {
+  const { avatar_url, name, location } = userData;
+
+  userAvatarElem.src = avatar_url;
+  userNameElem.textContent = name;
+  userLocationElem.textContent = location ? `from ${location}` : "";
+};
+
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInputElem = document.querySelector(".name-form__input");
+
+const onSearchUser = () => {
+  const userName = userNameInputElem.value;
+  fetchUserData(userName).then((userData) => renderUserDada(userData));
+};
+
+showUserBtnElem.addEventListener("click", onSearchUser);
