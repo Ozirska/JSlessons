8092072feb80cef043a7d9efba41c03a9267a39e
Ozirska <m.ozirska2000@gmail.com>
2022-12-30 12:03:59 +0100

    register form with server storage

---
+++ lesson34/task3/index.js
@@ -0,0 +1,47 @@
+const baseUrl = "https://63a97540100b7737b9928dc7.mockapi.io/users";
+
+const registerBtn = document.querySelector(".submit-button");
+
+const createData = (data) => {
+  const userData = {
+    email: data.email,
+    name: data.name,
+    password: data.password,
+  };
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userData),
+  });
+};
+const form = document.querySelector(".login-form");
+
+const disableBtn = () => {
+  if (form.reportValidity() === true) {
+    registerBtn.removeAttribute("disabled", "");
+  } else {
+    registerBtn.setAttribute("disabled", "disabled");
+  }
+};
+form.addEventListener("input", disableBtn);
+
+const cleanFild = () => {
+  const filds = document.querySelectorAll(".form-input");
+  [...filds].map((fild) => (fild.value = ""));
+};
+
+const getValue = (event) => {
+  event.preventDefault();
+
+  const formData = [...new FormData(form)].reduce(
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {}
+  );
+  createData(formData);
+  cleanFild();
+  registerBtn.setAttribute("disabled", "disabled");
+};
+
+registerBtn.addEventListener("click", getValue);
